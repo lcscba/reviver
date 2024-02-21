783ce753b8a8df123995c5c86b7c3a2fb20ca44f
lcscba <150151105+lcscba@users.noreply.github.com>
2024-02-20 20:27:47 -0400

    alterações
    layout feed e post, alteração nome do usuario

--- reviver/myapp/views.py
+++ reviver/myapp/views.py
@@ -78,8 +78,8 @@ def login_view(request):
             'incorrect_login': False
         })
     elif request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST["username"]
+        password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
